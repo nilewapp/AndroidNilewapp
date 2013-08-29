e976c732dd8aba90d0539466821bfb3afe2d5596
nilewapp <robert.welin@gmail.com>
2013-08-29 19:11:44 +0100

    Added getters to HttpException

--- src/com/mooo/nilewapps/androidnilewapp/HttpException.java
+++ src/com/mooo/nilewapps/androidnilewapp/HttpException.java
@@ -5,10 +5,28 @@ import org.apache.http.client.methods.HttpPost;
 
 @SuppressWarnings("serial")
 public class HttpException extends Exception {
-    HttpPost request;
-    HttpResponse response;
+    
+    private HttpPost request;
+    private HttpResponse response;
+    
     public HttpException(HttpPost request, HttpResponse response) {
         this.request = request;
         this.response = response;
     }
+    
+    @Override
+    public String getMessage() {
+        StringBuilder message = new StringBuilder("HttpException response code: ");
+        message.append(response.getStatusLine().getStatusCode());
+        return message.toString();
+    }
+
+    public HttpPost getRequest() {
+        return request;
+    }
+
+    public HttpResponse getResponse() {
+        return response;
+    }
+    
 }
